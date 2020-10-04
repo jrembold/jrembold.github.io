7a84b0d7b166bb5a709b2dd176c27f9b195d1850
Jed Rembold <jedediah.r@gmail.com>
2020-10-03 23:41:41 -0700

    Fix field issue and refresh needs
    Made it so the field auto-clears when you select it, and also cleaned up
    the moves array so that it gets cleaned out each time you submit a new
    entry, removing the random walks that you could get.

--- image_test.js
+++ image_test.js
@@ -1,7 +1,5 @@
 
 let img;
-let col;
-let pswrd;
 let moves = [];
 let mousecol;
 let rows = [];
@@ -14,10 +12,9 @@ function preload() {
 
 function setup() {
 	pixelDensity(1);
-	input = createInput('Enter Password:');
+	input = select('#inputfield');
 	button = createButton("Decode");
 	createCanvas(img.width,img.height);
-	col = color(255,255,0);
 	mouseview = createImage(300,300);
 	mousecol = createImage(img.width, img.height);
 	mousecol.copy(img, 0, 0, img.width, img.height, 0, 0, mousecol.width, mousecol.height);
@@ -28,30 +25,33 @@ function setup() {
 
 }
 
-function transcribeBlock(img) {
-	img.loadPixels();
-	const tmp = img.pixels.slice();
+function transcribeBlock(thisimage) {
+	thisimage.loadPixels();
+	const tmp = thisimage.pixels.slice();
 	let dir = 1;
-	for (let i = 0; i < img.width; i++) {
-		for (let j = 0; j < img.height; j++) {
+	for (let i = 0; i < thisimage.width; i++) {
+		for (let j = 0; j < thisimage.height; j++) {
 			shift = dir * moves[i % moves.length];
-			row = ((j + shift) % img.height + img.height) % img.height;
+			row = ((j + shift) % thisimage.height + thisimage.height) % thisimage.height;
 			rows.push(row);
-			index = 4 * ( j * img.width +  i);
-			shiftedindex = 4 * ( row * img.width +  i);
-			img.pixels[index]   = tmp[shiftedindex];
-			img.pixels[index+1] = tmp[shiftedindex+1];
-			img.pixels[index+2] = tmp[shiftedindex+2];
-			img.pixels[index+3] = 255;
+			index = 4 * ( j * thisimage.width +  i);
+			shiftedindex = 4 * ( row * thisimage.width +  i);
+			thisimage.pixels[index]   = tmp[shiftedindex];
+			thisimage.pixels[index+1] = tmp[shiftedindex+1];
+			thisimage.pixels[index+2] = tmp[shiftedindex+2];
+			thisimage.pixels[index+3] = 255;
 
 		}
 		dir = dir * -1;
 	}
-	img.updatePixels();
+	thisimage.updatePixels();
 }
 
 function decode() {
+	//mousecol = createImage(img.width, img.height);
+	mousecol.copy(img, 0, 0, img.width, img.height, 0, 0, mousecol.width, mousecol.height);
 	pswrd = input.value();
+	moves = []
 	for (i=0; i < pswrd.length; i++){
 		moves.push(30*pswrd.charCodeAt(i));
 	}
