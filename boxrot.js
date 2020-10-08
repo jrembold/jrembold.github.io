fd8467adbdeb21421581271810744d3993cdbd93
Jed Rembold <jedediah.r@gmail.com>
2020-10-07 18:05:59 -0700

    Add perfect tiling to boxrot and some opacity

--- boxrot.js
+++ boxrot.js
@@ -6,8 +6,8 @@ let size = 20;
 
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	background(50);
+	createCanvas(windowWidth-windowWidth%size, windowHeight-windowHeight%size);
+	background(40);
 	colorMode(HSB)
 	boxpx = windowWidth/2;
 	boxpy = windowHeight/2;
@@ -15,10 +15,14 @@ function setup() {
 
 function draw() {
 	//noStroke();
+	background(0,0.002);
 	fill(col,100,100);
 	rect(boxpx,boxpy, size, size);
-	boxpx = (boxpx + size*floor(random(-1,2)) + windowWidth) % windowWidth;
-	boxpy = (boxpy + size*floor(random(-1,2)) + windowHeight) % windowHeight;
+	if (random() < 0.5) {
+		boxpx = (boxpx + size*floor(random(-1,2)) + width) % width;
+	} else {
+		boxpy = (boxpy + size*floor(random(-1,2)) + height) % height;
+	}
 	col += .1;
 	if (col > 300) {
 		col = 0;
